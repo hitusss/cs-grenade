3eb0587df83844e2eb1f6289445550f78c3bdbef
Hitusss <kuba.niespielak@gmail.com>
2024-06-04 16:56:34 +0200

    chore: add imports sort plugin to prettier

---
+++ prettier.config.js
@@ -0,0 +1,26 @@
+import { default as defaultConfig } from '@epic-web/config/prettier'
+
+/** @type {import("prettier").Options} */
+export default {
+	...defaultConfig,
+	plugins: [...defaultConfig.plugins, '@ianvs/prettier-plugin-sort-imports'],
+	importOrder: [
+		'^(react/(.*)$)|^(react$)',
+		'^(@remix-run/(.*)$)|^(@remix-run$)',
+		'<THIRD_PARTY_MODULES>',
+		'',
+		'^types$',
+		'^#app/utils/(.*)$',
+		'^#app/components/ui/(.*)$',
+		'^#app/components/(.*)$',
+		'^#app/styles/(.*)$',
+		'^#app/routes/(.*)$',
+		'^#app/(.*)$',
+		'',
+		'^#(.*)$',
+		'',
+		'^[./]',
+	],
+	importOrderTypeScriptVersion: '5.4.5',
+	importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+}
