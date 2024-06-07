909364a5f0ad6b36c265cc7f5f649330a45c370a
Hitusss <kuba.niespielak@gmail.com>
2024-06-07 20:15:06 +0200

    chore: create MapsList component

---
+++ app/components/maps-list.tsx
@@ -0,0 +1,53 @@
+import { Link } from '@remix-run/react'
+
+import { cn } from '#app/utils/misc.tsx'
+
+import { grenadeTypes } from '#types/grenades-types.ts'
+import { teams } from '#types/teams.ts'
+
+import { Card, CardTitle } from './ui/card.tsx'
+
+export default function MapsList({
+	maps,
+}: {
+	maps: Array<{
+		name: string
+		label: string
+		isActive: boolean
+		imageId?: string
+		logoId?: string
+	}>
+}) {
+	return (
+		<ul className="flex flex-wrap mt-6 gap-6 px-8">
+			{maps.map(map => (
+				<li key={map.name}>
+					<Link
+						to={`/map/${map.name}/${teams[0]}/${grenadeTypes[0]}`}
+						className="group"
+					>
+						<Card
+							size="base"
+							className={cn(
+								'relative isolate overflow-hidden',
+								!map.isActive && 'grayscale-[0.75]',
+							)}
+						>
+							<img
+								src={`/resources/map-images/${map.imageId}`}
+								className="absolute h-full w-full object-cover -z-10 brightness-75 duration-500 transition-all group-hover:blur-sm group-hover:brightness-50 group-hover:scale-150"
+							/>
+							<img
+								src={`/resources/map-logos/${map.logoId}`}
+								className="transition-all duration-500 group-hover:scale-75"
+							/>
+							<CardTitle className="text-center text-white transition-all duration-500 group-hover:scale-125 group-hover:-translate-y-1/2">
+								{map.label}
+							</CardTitle>
+						</Card>
+					</Link>
+				</li>
+			))}
+		</ul>
+	)
+}
