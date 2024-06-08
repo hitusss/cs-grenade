40383f0cc75a0019604234f996adefbf3d6cc633
Hitusss <kuba.niespielak@gmail.com>
2024-06-08 19:50:51 +0200

    feat: add tooltip to map-nav

--- app/components/map-nav.tsx
+++ app/components/map-nav.tsx
@@ -1,6 +1,12 @@
 import { Link } from '@remix-run/react'
 
 import { Button } from './ui/button.tsx'
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from './ui/tooltip.tsx'
 
 export function MapNav({
 	label,
@@ -19,22 +25,29 @@ export function MapNav({
 	return (
 		<nav>
 			<p className="text-caption pl-2">{label}</p>
-			<ul className="flex gap-2 p-2 border rounded-md">
-				{items.map(i => (
-					<li key={i.value}>
-						<Button
-							asChild
-							variant={i.value === currentValue ? 'default' : 'secondary'}
-							size="icon"
-						>
-							<Link to={i.to}>
-								<img src={i.img} alt={i.label} className="size-7" />
-								<span className="sr-only">{i.label}</span>
-							</Link>
-						</Button>
-					</li>
-				))}
-			</ul>
+			<TooltipProvider>
+				<ul className="flex gap-2 p-2 border rounded-md">
+					{items.map(i => (
+						<li key={i.value}>
+							<Tooltip>
+								<TooltipTrigger asChild>
+									<Button
+										asChild
+										variant={i.value === currentValue ? 'default' : 'secondary'}
+										size="icon"
+									>
+										<Link to={i.to}>
+											<img src={i.img} alt={i.label} className="size-7" />
+											<span className="sr-only">{i.label}</span>
+										</Link>
+									</Button>
+								</TooltipTrigger>
+								<TooltipContent side="bottom">{i.label}</TooltipContent>
+							</Tooltip>
+						</li>
+					))}
+				</ul>
+			</TooltipProvider>
 		</nav>
 	)
 }
