efc4d0419353a6198efa7a92328aa6a7ceb40bbb
Hitusss <kuba.niespielak@gmail.com>
2024-06-08 20:11:42 +0200

    feat: create MapPage

--- app/routes/map+/$mapName+/$team+/$grenadeType+/index.tsx
+++ app/routes/map+/$mapName+/$team+/$grenadeType+/index.tsx
@@ -1,3 +1,22 @@
-export default function GrenadeMapPage() {
-	return <></>
+import { Link } from '@remix-run/react'
+
+import { userHasPermission } from '#app/utils/permissions.ts'
+import { useOptionalUser } from '#app/utils/user.ts'
+import { Button } from '#app/components/ui/button.tsx'
+
+export default function MapPage() {
+	const user = useOptionalUser()
+	const hasPermission = userHasPermission(user, 'create:destination')
+	console.log(user, hasPermission)
+	return (
+		<>
+			{user ? (
+				<Button className="absolute bottom-0 right-0 z-10" asChild>
+					<Link to="new">
+						{hasPermission ? 'Create' : 'Request'} new destination
+					</Link>
+				</Button>
+			) : null}
+		</>
+	)
 }
