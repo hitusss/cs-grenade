6bce091075d970c8c94633bf1620437dabbbdb65
Hitusss <kuba.niespielak@gmail.com>
2024-06-11 13:26:53 +0200

    feat: add destination markers render to map page

--- app/routes/map+/$mapName+/$team+/$type+/index.tsx
+++ app/routes/map+/$mapName+/$team+/$type+/index.tsx
@@ -1,10 +1,48 @@
-import { Link } from '@remix-run/react'
+import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { invariantResponse } from '@epic-web/invariant'
 
+import { prisma } from '#app/utils/db.server.ts'
 import { userHasPermission } from '#app/utils/permissions.ts'
 import { useOptionalUser } from '#app/utils/user.ts'
 import { Button } from '#app/components/ui/button.tsx'
+import { DestinationMarker } from '#app/components/destination-marker.tsx'
+
+export async function loader({ params }: LoaderFunctionArgs) {
+	const { mapName, team, type } = params
+
+	invariantResponse(mapName, 'Map name is required')
+	invariantResponse(team, 'Team is required')
+	invariantResponse(type, 'Grenade type is required')
+
+	const destinations = await prisma.destination.findMany({
+		where: {
+			type,
+			team,
+			verified: true,
+		},
+		select: {
+			id: true,
+			x: true,
+			y: true,
+			name: true,
+			grenades: {
+				where: {
+					verified: true,
+				},
+				select: {
+					id: true,
+				},
+			},
+		},
+	})
+
+	return json({ destinations })
+}
 
 export default function MapPage() {
+	const loaderData = useLoaderData<typeof loader>()
+
 	const user = useOptionalUser()
 	const hasPermission = userHasPermission(user, 'create:destination')
 
@@ -17,6 +55,18 @@ export default function MapPage() {
 					</Link>
 				</Button>
 			) : null}
+			{loaderData.destinations.map(d => (
+				<DestinationMarker
+					key={d.id}
+					to={d.id}
+					coords={{
+						x: d.x,
+						y: d.y,
+					}}
+					name={d.name}
+					count={d.grenades.length}
+				/>
+			))}
 		</>
 	)
 }
