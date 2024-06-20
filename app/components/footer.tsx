b66e513bdd4a13eba02d04cd979174da85f70ab9
Hitusss <kuba.niespielak@gmail.com>
2024-06-20 13:49:20 +0200

    feat: create footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,95 @@
+import { Link, useRouteLoaderData } from '@remix-run/react'
+
+import { cn } from '#app/utils/misc.tsx'
+import { type loader as rootLoader } from '#app/root.tsx'
+
 import { Logo } from './logo.tsx'
+import { Button } from './ui/button.tsx'
 
 export function Footer() {
+	const data = useRouteLoaderData<typeof rootLoader>('root')
+
+	const activeMaps = data?.maps.filter((m) => m.isActive) || []
+	const inactiveMaps = data?.maps.filter((m) => !m.isActive) || []
+
 	return (
-		<footer className="container flex justify-between pb-5">
-			<Logo className="size-12" />
+		<footer className="bg-accent">
+			<div className="container flex flex-wrap justify-evenly gap-12 py-8 sm:gap-24 sm:py-16">
+				<Logo className="size-36" />
+				<FooterSection title="Links">
+					<FooterLink to="/">Home</FooterLink>
+					<FooterLink to="/users">Users</FooterLink>
+					<FooterLink to="/support">Support</FooterLink>
+					<FooterLink to="/about">About</FooterLink>
+					<FooterLink to="/privacy">Privacy</FooterLink>
+					<FooterLink to="/tos">TOS</FooterLink>
+				</FooterSection>
+				<FooterSection title="Maps">
+					{activeMaps.map((m) => (
+						<FooterLink key={m.name} to={`/map/${m.name}`} className="gap-2">
+							<img
+								alt=""
+								src={`/resources/map-logos/${m.logo?.id}`}
+								className="size-6"
+							/>
+							{m.label}
+						</FooterLink>
+					))}
+				</FooterSection>
+				{inactiveMaps.length > 0 ? (
+					<FooterSection title="Inactive maps">
+						{inactiveMaps.map((m) => (
+							<FooterLink key={m.name} to={`/map/${m.name}`} className="gap-2">
+								<img
+									alt=""
+									src={`/resources/map-logos/${m.logo?.id}`}
+									className="size-6"
+								/>
+								{m.label}
+							</FooterLink>
+						))}
+					</FooterSection>
+				) : null}
+			</div>
+			<p className="px-4 text-right font-light text-muted-foreground">
+				<a href="https://github.com/hitusss">
+					All rights reserved © Hitusss
+					{new Date().getFullYear()}
+				</a>
+			</p>
 		</footer>
+	)
+}
+
+function FooterSection({
+	children,
+	title,
+}: {
+	children: React.ReactNode
+	title: string
+}) {
+	return (
+		<div className="grid w-full grid-rows-[auto_1fr] place-content-center sm:w-auto">
+			<p className="mb-2 font-bold">{title}</p>
+			<div className="flex max-h-full flex-col flex-wrap content-start gap-2 sm:max-h-36">
+				{children}
+			</div>
+		</div>
+	)
+}
+
+function FooterLink({
+	children,
+	to,
+	className,
+}: {
+	children: React.ReactNode
+	to: string
+	className?: string
+}) {
+	return (
+		<Button variant="link" asChild className={cn('justify-start', className)}>
+			<Link to={to}>{children}</Link>
+		</Button>
 	)
 }
