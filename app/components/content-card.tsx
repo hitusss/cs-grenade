278cb81df8ab2ed1beee225c71de02695deeab7d
Hitusss <kuba.niespielak@gmail.com>
2024-06-21 18:54:11 +0200

    chore: add ContentCard

---
+++ app/components/content-card.tsx
@@ -0,0 +1,55 @@
+import { cn } from '#app/utils/misc.tsx'
+
+import { Card, CardFooter, CardHeader } from './ui/card.tsx'
+
+type ContentCardProps = {
+	name: string
+	verified?: boolean
+	fields: {
+		label: string
+		img?: string
+		value: string
+	}[]
+}
+
+export function ContentCard({
+	name,
+	verified = true,
+	fields,
+}: ContentCardProps) {
+	return (
+		<Card
+			variant={verified ? 'default' : 'muted'}
+			size="lg"
+			className="flex flex-col justify-between break-words"
+		>
+			<CardHeader>
+				<p className="text-body-sm">Name</p>
+				<p className="text-h5">{name}</p>
+				{verified ? null : (
+					<p className="text-body-sm font-thin italic">Waiting for accept</p>
+				)}
+			</CardHeader>
+			<CardFooter className="flex-col items-start">
+				{fields.map((f) => (
+					<div
+						key={f.label}
+						className="flex items-center justify-between gap-2 w-full flex-wrap"
+					>
+						<p className="text-body-sm">{f.label}</p>
+						<div className="flex items-center gap-1">
+							{f.img ? (
+								<img
+									src={f.img}
+									alt=""
+									className={cn('size-5', !verified && 'opacity-75')}
+								/>
+							) : null}
+							<p className="text-body-sm font-semibold">{f.value}</p>
+						</div>
+					</div>
+				))}
+			</CardFooter>
+		</Card>
+	)
+}
