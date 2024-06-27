1ed249599b23a11751cb779ed50868116a6e23fc
Hitusss <kuba.niespielak@gmail.com>
2024-06-27 12:11:00 +0200

    feat: add events route

---
+++ app/routes/events.$.tsx
@@ -0,0 +1,24 @@
+import { EventEmitter } from 'events'
+import { type LoaderFunctionArgs } from '@remix-run/node'
+import { invariantResponse } from '@epic-web/invariant'
+import { eventStream } from 'remix-utils/sse/server'
+
+export const emitter = new EventEmitter()
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	const event = params['*']
+	invariantResponse(event, 'Event is required')
+
+	return eventStream(request.signal, (send) => {
+		const handler = () => {
+			send({
+				data: new Date().toISOString(),
+			})
+		}
+
+		emitter.addListener(event, handler)
+		return () => {
+			emitter.removeListener(event, handler)
+		}
+	})
+}
