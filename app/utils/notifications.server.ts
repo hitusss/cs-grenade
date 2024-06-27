72002dfea80eaa15e7bc03357324d0e7eb2a7e7a
Hitusss <kuba.niespielak@gmail.com>
2024-06-27 12:28:09 +0200

    feat: add notify util

---
+++ app/utils/notifications.server.ts
@@ -0,0 +1,26 @@
+import { emitter } from '#app/routes/events.$.ts'
+
+import { prisma } from './db.server.ts'
+
+export async function notify({
+	userId,
+	title,
+	description,
+	redirectTo,
+}: {
+	userId: string
+	title: string
+	description?: string
+	redirectTo?: string
+}) {
+	await prisma.notification.create({
+		data: {
+			userId,
+			title,
+			description,
+			redirectTo,
+		},
+	})
+
+	emitter.emit(`/notifications/${userId}`)
+}
