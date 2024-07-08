7a44a2a1e1668ccd4a6867cfbd103be978c4a93f
Hitusss <kuba.niespielak@gmail.com>
2024-07-08 18:40:04 +0200

    refactor: move MessageSchema to seperate validator

---
+++ app/utils/validators/message.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod'
+
+export const MAX_SIZE = 1024 * 1024 * 2 // 2MB
+
+export const MessageSchema = z.object({
+	message: z
+		.string({
+			invalid_type_error: 'Message should be a string',
+			required_error: 'Message is required',
+		})
+		.min(3, 'Message should be longer then 2 chars')
+		.max(1280, 'Message should be not longer then 1280 chars'),
+	images: z
+		.array(
+			z.instanceof(File, {
+				message: 'Image is required',
+			}),
+		)
+		.max(5, `You can't add more then 5 images`)
+		.refine(
+			(files) =>
+				files &&
+				files.every((file) =>
+					file.size > 0
+						? ['image/jpeg', 'image/png'].includes(file.type)
+						: true,
+				),
+			'Image should be a PNG or JPG',
+		)
+		.refine(
+			(files) => files && files.every((file) => file.size <= MAX_SIZE),
+			'Image size must be less than 2MB',
+		),
+})
--- app/utils/validators/support.ts
+++ app/utils/validators/support.ts
@@ -1,37 +1,6 @@
 import { z } from 'zod'
 
-export const MAX_SIZE = 1024 * 1024 * 2 // 2MB
-
-export const TicketMessageSchema = z.object({
-	message: z
-		.string({
-			invalid_type_error: 'Message should be a string',
-			required_error: 'Message is required',
-		})
-		.min(3, 'Message should be longer then 2 chars')
-		.max(1280, 'Message should be not longer then 1280 chars'),
-	images: z
-		.array(
-			z.instanceof(File, {
-				message: 'Image is required',
-			}),
-		)
-		.max(5, `You can't add more then 5 images`)
-		.refine(
-			(files) =>
-				files &&
-				files.every((file) =>
-					file.size > 0
-						? ['image/jpeg', 'image/png'].includes(file.type)
-						: true,
-				),
-			'Image should be a PNG or JPG',
-		)
-		.refine(
-			(files) => files && files.every((file) => file.size <= MAX_SIZE),
-			'Image size must be less than 2MB',
-		),
-})
+import { MAX_SIZE, MessageSchema } from './message.ts'
 
 export const NewTicketSchema = z
 	.object({
@@ -43,4 +12,6 @@ export const NewTicketSchema = z
 			.min(3, 'Title should be longer then 2 chars')
 			.max(24, 'Title should be not longer then 24 chars'),
 	})
-	.and(TicketMessageSchema)
+	.and(MessageSchema)
+
+export { MessageSchema as TicketMessageSchema, MAX_SIZE }
