8336a2aa53cf90de360f082df1cdf2921e05361b
Hitusss <kuba.niespielak@gmail.com>
2024-07-11 16:55:56 +0200

    feat: create admin destinations requests routes

---
+++ app/routes/admin+/requests+/destinations.tsx
@@ -0,0 +1,288 @@
+import { json, redirect, type LoaderFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData, useLocation } from '@remix-run/react'
+import { type SEOHandle } from '@nasa-gcn/remix-seo'
+import { type Prisma } from '@prisma/client'
+import { type ColumnDef } from '@tanstack/react-table'
+
+import { grenadeLabels } from '#types/grenades-types.ts'
+import { teamLabels } from '#types/teams.ts'
+import { prisma } from '#app/utils/db.server.ts'
+import { requireUserWithPermission } from '#app/utils/permissions.server.ts'
+import { getUserImgSrc } from '#app/utils/user.ts'
+import { Button } from '#app/components/ui/button.tsx'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '#app/components/ui/dropdown-menu.tsx'
+import { Icon } from '#app/components/ui/icon.tsx'
+import { ContentFilter } from '#app/components/content-filter.tsx'
+import {
+	DataTable,
+	DataTableColumnHeader,
+	SortSchema,
+} from '#app/components/data-table.tsx'
+import { Pagination } from '#app/components/pagination.tsx'
+
+const columns: ColumnDef<{
+	id: string
+	team: string
+	type: string
+	name: string
+	map: {
+		name: string
+		label: string
+		logo: {
+			id: string
+		} | null
+	}
+	user: {
+		name: string | null
+		image: {
+			id: string
+		} | null
+		username: string
+	}
+}>[] = [
+	{
+		accessorKey: 'name',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Name" />
+		),
+		enableSorting: true,
+		enableHiding: false,
+	},
+	{
+		accessorKey: 'map',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Map" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={`/resources/map-logos/${row.original.map.logo?.id}`}
+					className="size-6 rounded-full"
+				/>
+				{row.original.map.label}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'team',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Team" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={`/img/teams/${row.original.team}.png`}
+					className="size-6 rounded-full"
+				/>
+				{teamLabels[row.original.team as keyof typeof teamLabels]}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'type',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Type" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={`/img/grenades/${row.original.type}.png`}
+					className="size-6 rounded-full"
+				/>
+				{grenadeLabels[row.original.type as keyof typeof grenadeLabels]}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'user',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="User" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={getUserImgSrc(row.original.user.image?.id)}
+					className="size-6 rounded-full"
+					alt={row.original.user.username}
+				/>
+				{row.original.user.name
+					? `${row.original.user.name} (${row.original.user.username})`
+					: row.original.user.username}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		id: 'actions',
+		cell: ({ row }) => {
+			const location = useLocation()
+			const params = new URLSearchParams({
+				redirectTo: `${location.pathname}?${location.search}`,
+			})
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<Icon name="ellipsis" className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<Link
+								to={`/map/${row.original.map.name}/${row.original.team}/${row.original.type}/${row.original.id}/admin/request?${params}`}
+								className="flex items-center gap-2"
+							>
+								Go to destination
+								<Icon name="arrow-right" />
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<Link
+								to={`/users/${row.original.user.username}`}
+								className="flex items-center gap-2"
+							>
+								Go to user profile
+								<Icon name="arrow-right" />
+							</Link>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		},
+		enableSorting: false,
+		enableHiding: false,
+	},
+]
+
+export const handle: SEOHandle = {
+	getSitemapEntries: () => null,
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requireUserWithPermission(
+		request,
+		'read:review-destination-request:any',
+	)
+
+	const searchParams = new URL(request.url).searchParams
+
+	const query = searchParams.get('query') ?? undefined
+	const map = searchParams.get('map') ?? undefined
+	const team = searchParams.get('team') ?? undefined
+	const type = searchParams.get('type') ?? undefined
+
+	const page = Number(searchParams.get('page') ?? 1)
+	const perPage = Number(searchParams.get('perPage') ?? 25)
+
+	const sort = searchParams.get('sort') ?? '[]'
+	const sortResult = SortSchema.safeParse(JSON.parse(sort))
+	if (!sortResult.success) {
+		searchParams.delete('sort')
+		return redirect(`/admin/requests/destinations?${searchParams.toString()}`)
+	}
+	const orderBy: Record<
+		string,
+		Prisma.SortOrder | Record<string, Prisma.SortOrder>
+	> = {}
+	sortResult.data.forEach((sort) => {
+		switch (sort.id) {
+			case 'map': {
+				orderBy[sort.id] = {
+					name: sort.desc ? 'desc' : 'asc',
+				}
+				break
+			}
+			case 'user': {
+				orderBy[sort.id] = {
+					name: sort.desc ? 'desc' : 'asc',
+				}
+				break
+			}
+			default: {
+				orderBy[sort.id] = sort.desc ? 'desc' : 'asc'
+			}
+		}
+	})
+
+	const total = await prisma.destination.count({
+		where: {
+			verified: false,
+			name: { contains: query },
+			mapName: map,
+			team,
+			type,
+		},
+	})
+	const destinations = await prisma.destination.findMany({
+		where: {
+			verified: false,
+			name: { contains: query },
+			mapName: map,
+			team,
+			type,
+		},
+		select: {
+			id: true,
+			name: true,
+			map: {
+				select: {
+					name: true,
+					label: true,
+					logo: {
+						select: {
+							id: true,
+						},
+					},
+				},
+			},
+			team: true,
+			type: true,
+			user: {
+				select: {
+					name: true,
+					username: true,
+					image: {
+						select: {
+							id: true,
+						},
+					},
+				},
+			},
+		},
+		orderBy,
+		take: perPage,
+		skip: page * perPage - perPage,
+	})
+
+	return json({ destinations, total })
+}
+
+export default function DestinationsAdminRoute() {
+	const data = useLoaderData<typeof loader>()
+
+	return (
+		<>
+			<h1>Destination Requests</h1>
+			<ContentFilter hideFilter={{ verified: true }} />
+			<DataTable columns={columns} data={data.destinations} />
+			<Pagination total={data.total} />
+		</>
+	)
+}
---
+++ app/routes/map+/$mapName+/$team+/$type+/_destination+/$destination+/admin+/request.tsx
@@ -0,0 +1,166 @@
+import { json, type LoaderFunctionArgs } from '@remix-run/node'
+import { Form, useLoaderData, useSearchParams } from '@remix-run/react'
+import { invariantResponse } from '@epic-web/invariant'
+
+import { prisma } from '#app/utils/db.server.ts'
+import { notify } from '#app/utils/notifications.server.ts'
+import { requireUserWithPermission } from '#app/utils/permissions.server.ts'
+import { redirectWithToast } from '#app/utils/toast.server.ts'
+import { Button } from '#app/components/ui/button.tsx'
+import { DestinationMarker } from '#app/components/destination-marker.tsx'
+import { MapBackButton, MapTitle } from '#app/components/map.tsx'
+
+import { type MapHandle } from '../../../_layout.tsx'
+
+const DEFAULT_REDIRECT_TO = '/admin/requests/destinations'
+
+export const handle: MapHandle = {
+	map: {
+		disableAllDestinations: true,
+	},
+}
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	await requireUserWithPermission(
+		request,
+		'create:review-destination-request:any',
+	)
+
+	const { destination: destinationId } = params
+
+	invariantResponse(destinationId, 'Destination is required')
+
+	const destination = await prisma.destination.findUnique({
+		where: {
+			id: destinationId,
+			verified: false,
+		},
+		select: {
+			name: true,
+			x: true,
+			y: true,
+		},
+	})
+
+	invariantResponse(destination, 'Not fount', { status: 404 })
+
+	return json({ destination })
+}
+
+export async function action({ request, params }: LoaderFunctionArgs) {
+	await requireUserWithPermission(
+		request,
+		'create:review-destination-request:any',
+	)
+
+	const { destination: destinationId } = params
+
+	invariantResponse(destinationId, 'Destination is required')
+
+	const destination = await prisma.destination.findUnique({
+		where: {
+			id: destinationId,
+		},
+		select: {
+			name: true,
+			mapName: true,
+			team: true,
+			type: true,
+			verified: true,
+			userId: true,
+		},
+	})
+
+	invariantResponse(destination, 'Not fount', { status: 404 })
+
+	const searchParams = new URL(request.url).searchParams
+	const redirectTo = searchParams.get('redirectTo') ?? DEFAULT_REDIRECT_TO
+
+	if (destination.verified) {
+		return redirectWithToast(redirectTo, {
+			type: 'error',
+			title: 'Destination already verified',
+			description: ``,
+		})
+	}
+
+	const formData = await request.formData()
+
+	const intent = formData.get('intent')
+
+	switch (intent) {
+		case 'accept': {
+			await prisma.destination.update({
+				where: {
+					id: destinationId,
+				},
+				data: {
+					verified: true,
+				},
+			})
+			notify({
+				userId: destination.userId,
+				title: 'Destination request accepted',
+				description: `Your destination request for ${destination.name} has been accepted`,
+				redirectTo: `/map/${destination.mapName}/${destination.team}/${destination.type}/${destinationId}`,
+			})
+			break
+		}
+		case 'reject': {
+			await prisma.destination.delete({
+				where: {
+					id: destinationId,
+				},
+			})
+			notify({
+				userId: destination.userId,
+				title: 'Destination request rejected',
+				description: `Your destination request for ${destination.name} has been rejected`,
+			})
+			break
+		}
+		default: {
+			throw new Response('Invalid intent', { status: 400 })
+		}
+	}
+
+	return redirectWithToast(redirectTo, {
+		type: 'success',
+		title: 'Destination request reviewed',
+		description: ``,
+	})
+}
+
+export default function AdminDestinationRequestRoute() {
+	const data = useLoaderData<typeof loader>()
+	const [searchParams] = useSearchParams()
+
+	const redirectTo = searchParams.get('redirectTo') ?? DEFAULT_REDIRECT_TO
+
+	return (
+		<>
+			<MapBackButton to={redirectTo} />
+			<MapTitle>{data.destination.name}</MapTitle>
+			<DestinationMarker
+				to=""
+				name={data.destination.name}
+				coords={{ x: data.destination.x, y: data.destination.y }}
+				highlight
+				disabled
+			/>
+			<Form method="POST" className="absolute bottom-0 right-0 z-10">
+				<Button
+					type="submit"
+					name="intent"
+					value="reject"
+					variant="destructive"
+				>
+					Reject
+				</Button>
+				<Button type="submit" name="intent" value="accept">
+					Accept
+				</Button>
+			</Form>
+		</>
+	)
+}
