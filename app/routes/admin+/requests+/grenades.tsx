6044ec462d6f1d9f3d4d47355f69fb2d270447c1
Hitusss <kuba.niespielak@gmail.com>
2024-07-12 18:59:39 +0200

    feat: create admin grenades requests routes

---
+++ app/routes/admin+/requests+/grenades.tsx
@@ -0,0 +1,310 @@
+import { json, redirect, type LoaderFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData, useLocation } from '@remix-run/react'
+import { type SEOHandle } from '@nasa-gcn/remix-seo'
+import { type Prisma } from '@prisma/client'
+import { type ColumnDef } from '@tanstack/react-table'
+
+import { grenadeLabels } from '#types/grenades-types.ts'
+import { teamLabels } from '#types/teams.ts'
+import { prisma } from '#app/utils/db.server.ts'
+import { requireUserWithPermission } from '#app/utils/permissions.server.ts'
+import { getUserImgSrc } from '#app/utils/user.ts'
+import { Button } from '#app/components/ui/button.tsx'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '#app/components/ui/dropdown-menu.tsx'
+import { Icon } from '#app/components/ui/icon.tsx'
+import { ContentFilter } from '#app/components/content-filter.tsx'
+import {
+	DataTable,
+	DataTableColumnHeader,
+	SortSchema,
+} from '#app/components/data-table.tsx'
+import { Pagination } from '#app/components/pagination.tsx'
+
+const columns: ColumnDef<{
+	type: string
+	id: string
+	team: string
+	name: string
+	map: {
+		name: string
+		label: string
+		logo: {
+			id: string
+		} | null
+	}
+	destination: {
+		id: string
+		name: string
+	}
+	user: {
+		name: string | null
+		image: {
+			id: string
+		} | null
+		username: string
+	}
+}>[] = [
+	{
+		accessorKey: 'name',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Name" />
+		),
+		enableSorting: true,
+		enableHiding: false,
+	},
+	{
+		accessorKey: 'destination',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Destination" />
+		),
+		cell: ({ row }) => row.original.destination.name,
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'map',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Map" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={`/resources/map-logos/${row.original.map.logo?.id}`}
+					className="size-6 rounded-full"
+				/>
+				{row.original.map.label}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'team',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Team" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={`/img/teams/${row.original.team}.png`}
+					className="size-6 rounded-full"
+				/>
+				{teamLabels[row.original.team as keyof typeof teamLabels]}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'type',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Type" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={`/img/grenades/${row.original.type}.png`}
+					className="size-6 rounded-full"
+				/>
+				{grenadeLabels[row.original.type as keyof typeof grenadeLabels]}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		accessorKey: 'user',
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="User" />
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center gap-2">
+				<img
+					src={getUserImgSrc(row.original.user.image?.id)}
+					className="size-6 rounded-full"
+					alt={row.original.user.username}
+				/>
+				{row.original.user.name
+					? `${row.original.user.name} (${row.original.user.username})`
+					: row.original.user.username}
+			</div>
+		),
+		enableSorting: true,
+		enableHiding: true,
+	},
+	{
+		id: 'actions',
+		cell: ({ row }) => {
+			const location = useLocation()
+			const params = new URLSearchParams({
+				redirectTo: `${location.pathname}?${location.search}`,
+			})
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<Icon name="ellipsis" className="h-4 w-4" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem asChild>
+							<Link
+								to={`/map/${row.original.map.name}/${row.original.team}/${row.original.type}/${row.original.destination.id}/${row.original.id}/admin/request?${params}`}
+								className="flex items-center gap-2"
+							>
+								Go to grenade
+								<Icon name="arrow-right" />
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem asChild>
+							<Link
+								to={`/users/${row.original.user.username}`}
+								className="flex items-center gap-2"
+							>
+								Go to user profile
+								<Icon name="arrow-right" />
+							</Link>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		},
+		enableSorting: false,
+		enableHiding: false,
+	},
+]
+
+export const handle: SEOHandle = {
+	getSitemapEntries: () => null,
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	await requireUserWithPermission(request, 'read:review-grenade-request:any')
+
+	const searchParams = new URL(request.url).searchParams
+
+	const query = searchParams.get('query') ?? undefined
+	const map = searchParams.get('map') ?? undefined
+	const team = searchParams.get('team') ?? undefined
+	const type = searchParams.get('type') ?? undefined
+
+	const page = Number(searchParams.get('page') ?? 1)
+	const perPage = Number(searchParams.get('perPage') ?? 25)
+
+	const sort = searchParams.get('sort') ?? '[]'
+	const sortResult = SortSchema.safeParse(JSON.parse(sort))
+	if (!sortResult.success) {
+		searchParams.delete('sort')
+		return redirect(`/admin/requests/grenades?${searchParams.toString()}`)
+	}
+	const orderBy: Record<
+		string,
+		Prisma.SortOrder | Record<string, Prisma.SortOrder>
+	> = {}
+	sortResult.data.forEach((sort) => {
+		switch (sort.id) {
+			case 'destination': {
+				orderBy[sort.id] = {
+					name: sort.desc ? 'desc' : 'asc',
+				}
+				break
+			}
+			case 'map': {
+				orderBy[sort.id] = {
+					name: sort.desc ? 'desc' : 'asc',
+				}
+				break
+			}
+			case 'user': {
+				orderBy[sort.id] = {
+					name: sort.desc ? 'desc' : 'asc',
+				}
+				break
+			}
+			default: {
+				orderBy[sort.id] = sort.desc ? 'desc' : 'asc'
+			}
+		}
+	})
+
+	const total = await prisma.grenade.count({
+		where: {
+			verified: false,
+			name: { contains: query },
+			mapName: map,
+			team,
+			type,
+		},
+	})
+	const grenades = await prisma.grenade.findMany({
+		where: {
+			verified: false,
+			name: { contains: query },
+			mapName: map,
+			team,
+			type,
+		},
+		select: {
+			id: true,
+			name: true,
+			destination: {
+				select: {
+					id: true,
+					name: true,
+				},
+			},
+			map: {
+				select: {
+					name: true,
+					label: true,
+					logo: {
+						select: {
+							id: true,
+						},
+					},
+				},
+			},
+			team: true,
+			type: true,
+			user: {
+				select: {
+					name: true,
+					username: true,
+					image: {
+						select: {
+							id: true,
+						},
+					},
+				},
+			},
+		},
+		orderBy,
+		take: perPage,
+		skip: page * perPage - perPage,
+	})
+
+	return json({ grenades, total })
+}
+
+export default function GrenadeAdminRoute() {
+	const data = useLoaderData<typeof loader>()
+
+	return (
+		<>
+			<h2>Grenade Requests</h2>
+			<ContentFilter hideFilter={{ verified: true }} />
+			<DataTable columns={columns} data={data.grenades} />
+			<Pagination total={data.total} />
+		</>
+	)
+}
---
+++ app/routes/map+/$mapName+/$team+/$type+/_destination+/$destination+/_grenade+/$grenade+/admin+/request.tsx
@@ -0,0 +1,216 @@
+import {
+	json,
+	type ActionFunctionArgs,
+	type LoaderFunctionArgs,
+} from '@remix-run/node'
+import { Form, useLoaderData, useSearchParams } from '@remix-run/react'
+import { invariantResponse } from '@epic-web/invariant'
+
+import { prisma } from '#app/utils/db.server.ts'
+import { notify } from '#app/utils/notifications.server.ts'
+import { requireUserWithPermission } from '#app/utils/permissions.server.ts'
+import { redirectWithToast } from '#app/utils/toast.server.ts'
+import { Button } from '#app/components/ui/button.tsx'
+import {
+	Dialog,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTrigger,
+} from '#app/components/ui/dialog.tsx'
+import { GrenadeMarker } from '#app/components/grenade-marker.tsx'
+import { useLightbox } from '#app/components/lightbox.tsx'
+import { MapBackButton, MapTitle } from '#app/components/map.tsx'
+
+import { type MapHandle } from '../../../../../_layout.tsx'
+
+const DEFAULT_REDIRECT_TO = '/admin/requests/grenades'
+
+export const handle: MapHandle = {
+	map: {
+		currentDestination: true,
+		disableAllGrenades: true,
+	},
+}
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+	await requireUserWithPermission(request, 'create:review-grenade-request:any')
+
+	const { grenade: grenadeId } = params
+
+	invariantResponse(grenadeId, 'Grenade is required')
+
+	const grenade = await prisma.grenade.findUnique({
+		where: {
+			id: grenadeId,
+			verified: false,
+		},
+		select: {
+			name: true,
+			description: true,
+			images: {
+				select: {
+					id: true,
+					description: true,
+				},
+			},
+			x: true,
+			y: true,
+		},
+	})
+
+	invariantResponse(grenade, 'Not fount', { status: 404 })
+
+	return json({ grenade })
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
+	await requireUserWithPermission(request, 'create:review-grenade-request:any')
+
+	const { grenade: grenadeId } = params
+
+	invariantResponse(grenadeId, 'Grenade is required')
+
+	const grenade = await prisma.grenade.findUnique({
+		where: {
+			id: grenadeId,
+		},
+		select: {
+			name: true,
+			destinationId: true,
+			mapName: true,
+			team: true,
+			type: true,
+			verified: true,
+			userId: true,
+		},
+	})
+
+	invariantResponse(grenade, 'Not fount', { status: 404 })
+
+	const searchParams = new URL(request.url).searchParams
+	const redirectTo = searchParams.get('redirectTo') ?? DEFAULT_REDIRECT_TO
+
+	if (grenade.verified) {
+		return redirectWithToast(redirectTo, {
+			type: 'error',
+			title: 'Grenade already verified',
+			description: ``,
+		})
+	}
+
+	const formData = await request.formData()
+
+	const intent = formData.get('intent')
+
+	switch (intent) {
+		case 'accept': {
+			await prisma.grenade.update({
+				where: {
+					id: grenadeId,
+				},
+				data: {
+					verified: true,
+				},
+			})
+			notify({
+				userId: grenade.userId,
+				title: 'Grenade request accepted',
+				description: `Your grenade request for ${grenade.name} has been accepted`,
+				redirectTo: `/map/${grenade.mapName}/${grenade.team}/${grenade.type}/${grenade.destinationId}/${grenadeId}`,
+			})
+			break
+		}
+		case 'reject': {
+			await prisma.grenade.delete({
+				where: {
+					id: grenadeId,
+				},
+			})
+			notify({
+				userId: grenade.userId,
+				title: 'Grenade request rejected',
+				description: `Your grenade request for ${grenade.name} has been rejected`,
+			})
+			break
+		}
+		default: {
+			throw new Response('Invalid intent', { status: 400 })
+		}
+	}
+
+	return redirectWithToast(redirectTo, {
+		type: 'success',
+		title: 'Grenade request reviewed',
+		description: ``,
+	})
+}
+
+export default function AdminGrenadeRequestRoute() {
+	const data = useLoaderData<typeof loader>()
+	const [searchParams] = useSearchParams()
+
+	const { openLightbox } = useLightbox()
+
+	const redirectTo = searchParams.get('redirectTo') ?? DEFAULT_REDIRECT_TO
+
+	const grenadeImages = data.grenade.images.map((image) => ({
+		src: `/resources/grenade-images/${image.id}`,
+		alt: image.description ?? undefined,
+		caption: image.description ?? undefined,
+	}))
+
+	return (
+		<>
+			<MapBackButton to={redirectTo} />
+			<MapTitle>{data.grenade.name}</MapTitle>
+			<GrenadeMarker
+				to=""
+				destination={{ x: data.grenade.x, y: data.grenade.y }}
+				coords={{ x: data.grenade.x, y: data.grenade.y }}
+				name={data.grenade.name}
+				highlight
+				disabled
+			/>
+
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button className="absolute bottom-0 right-0 z-10">
+						Show content
+					</Button>
+				</DialogTrigger>
+				<DialogContent className="max-h-[90vh] max-w-5xl overflow-auto">
+					<DialogHeader>{data.grenade.name}</DialogHeader>
+					<p>{data.grenade.description}</p>
+					<ul className="flex flex-wrap gap-4">
+						{data.grenade.images.map((image, index) => (
+							<li key={image.id} className="w-48 space-y-1 md:w-64">
+								<img
+									src={`/resources/grenade-images/${image.id}`}
+									className="aspect-square cursor-pointer object-cover shadow-md"
+									onClick={() => openLightbox({ index, images: grenadeImages })}
+								/>
+								<p>{image.description}</p>
+							</li>
+						))}
+					</ul>
+					<DialogFooter>
+						<Form method="POST" className="flex gap-4">
+							<Button
+								type="submit"
+								name="intent"
+								value="reject"
+								variant="destructive"
+							>
+								Reject
+							</Button>
+							<Button type="submit" name="intent" value="accept">
+								Accept
+							</Button>
+						</Form>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</>
+	)
+}
