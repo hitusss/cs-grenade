7b4e253dad5f8f0149594e97ea10908fb6ccefbb
Hitusss <kuba.niespielak@gmail.com>
2024-07-17 15:13:44 +0200

    feat: add name and no changes state to DiffView

--- app/components/diff.tsx
+++ app/components/diff.tsx
@@ -3,20 +3,24 @@ import { cn } from '#app/utils/misc.js'
 import { Icon } from './ui/icon.tsx'
 
 type DiffViewProps = {
+	name: string
 	oldValue?: React.ReactNode
 	newValue?: React.ReactNode
 	deleted?: boolean
 }
 
-export function DiffView({ oldValue, newValue, deleted }: DiffViewProps) {
+export function DiffView({ name, oldValue, newValue, deleted }: DiffViewProps) {
 	return (
-		<div className="flex flex-wrap [&>*]:flex-1">
-			{oldValue ? (
-				<DiffCell isDeleted={deleted || newValue !== undefined}>
+		<div>
+			<p className="text-caption">{name}</p>
+			<div className="flex flex-wrap [&>*]:flex-1">
+				<DiffCell
+					isDeleted={Boolean(oldValue) && (deleted || newValue !== undefined)}
+				>
 					{oldValue}
 				</DiffCell>
-			) : null}
-			{newValue ? <DiffCell isNew>{newValue}</DiffCell> : null}
+				<DiffCell isNew={Boolean(newValue)}>{newValue}</DiffCell>
+			</div>
 		</div>
 	)
 }
@@ -32,18 +36,21 @@ function DiffCell({
 }) {
 	return (
 		<div
-			className={cn('flex items-center gap-1 border-b-2 border-t-2 p-1', {
+			className={cn('flex items-center gap-1 p-1', {
+				'border-b-2 border-t-2': isNew || isDeleted,
 				'border-diff-green bg-diff-green/50': isNew,
 				'border-diff-red bg-diff-red/50': isDeleted,
 			})}
 		>
-			<Icon
-				name={isNew ? 'plus' : 'minus'}
-				className={cn({
-					'text-diff-green': isNew,
-					'text-diff-red': isDeleted,
-				})}
-			/>
+			{isNew || isDeleted ? (
+				<Icon
+					name={isNew ? 'plus' : 'minus'}
+					className={cn({
+						'text-diff-green': isNew,
+						'text-diff-red': isDeleted,
+					})}
+				/>
+			) : null}
 			{children}
 		</div>
 	)
