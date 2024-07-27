625c362a8a3bf2367fd69ea8f19432806e1e613e
Hitusss <kuba.niespielak@gmail.com>
2024-07-27 15:34:30 +0200

    chore: change remaining npm calls to pnpm

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 	],
 
 	webServer: {
-		command: process.env.CI ? 'npm run start:mocks' : 'npm run dev',
+		command: process.env.CI ? 'pnpm run start:mocks' : 'pnpm run dev',
 		port: Number(PORT),
 		reuseExistingServer: true,
 		stdout: 'pipe',
