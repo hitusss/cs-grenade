9f47e412c7a76a751c2128e5fd8dd400c027f3ba
Hitusss <kuba.niespielak@gmail.com>
2024-11-03 19:38:05 +0100

    feat: update ContentFilder to use Collapsible and Card instead of Accordion

--- app/components/content-filter.tsx
+++ app/components/content-filter.tsx
@@ -9,13 +9,13 @@ import { teamLabels, teams } from '#types/teams.ts'
 import { useDebounce } from '#app/utils/misc.tsx'
 import { type loader as rootLoader } from '#app/root.tsx'
 
-import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from './ui/accordion.tsx'
 import { Button } from './ui/button.tsx'
+import { Card, CardContent } from './ui/card.tsx'
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from './ui/collapsible.tsx'
 import { Icon } from './ui/icon.tsx'
 import { Input } from './ui/input.tsx'
 import {
@@ -91,59 +91,52 @@ export function ContentFilter({ hideFilter }: ContentFilterProps) {
 	}, 400)
 
 	return (
-		<Accordion type="single" collapsible>
-			<AccordionItem value="filters">
-				<AccordionTrigger>Filters</AccordionTrigger>
-				<AccordionContent className="flex flex-col gap-4 p-2">
-					<div className="flex flex-wrap gap-4 [&>*]:w-full md:[&>*]:w-64">
-						{hideFilter?.query ? null : (
-							<Input
-								type="search"
-								name="query"
-								placeholder="Search"
-								defaultValue={query}
-								onChange={(e) => handleQueryChange(e.currentTarget.value)}
-							/>
-						)}
-						{hideFilter?.map ? null : (
-							<Select
-								value={map}
-								onValueChange={(v) => handleFilterChange('map', v)}
-							>
-								<div className="flex">
-									<SelectTrigger>
-										<SelectValue placeholder="Map" />
-									</SelectTrigger>
-									<Button
-										variant="ghost"
-										size="icon"
-										disabled={!map}
-										onClick={() => handleFilterDelete('map')}
-									>
-										<Icon name="x" />
-										<span className="sr-only">clean map</span>
-									</Button>
-								</div>
-								<SelectContent>
-									<SelectGroup>
-										<SelectLabel>Active</SelectLabel>
-										{activeMaps.map((m) => (
-											<SelectItem key={m.label} value={m.name}>
-												<div className="flex items-center gap-2 truncate text-sm">
-													<img
-														src={`/resources/map-logos/${m.logo?.id}`}
-														alt=""
-														className="size-4"
-													/>
-													{m.label}
-												</div>
-											</SelectItem>
-										))}
-									</SelectGroup>
-									{inactiveMaps.length > 0 ? (
+		<Collapsible className="group/collapsible">
+			<CollapsibleTrigger asChild>
+				<Button variant="outline" size="lg" className="w-full justify-between">
+					Filters
+					<Icon
+						name="chevron-down"
+						className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-180"
+					/>
+				</Button>
+			</CollapsibleTrigger>
+			<CollapsibleContent asChild>
+				<Card className="mt-2">
+					<CardContent className="flex flex-col gap-4 p-6">
+						<div className="flex flex-wrap gap-4 [&>*]:w-full md:[&>*]:w-64">
+							{hideFilter?.query ? null : (
+								<Input
+									type="search"
+									name="query"
+									placeholder="Search"
+									defaultValue={query}
+									onChange={(e) => handleQueryChange(e.currentTarget.value)}
+								/>
+							)}
+							{hideFilter?.map ? null : (
+								<Select
+									value={map}
+									onValueChange={(v) => handleFilterChange('map', v)}
+								>
+									<div className="flex">
+										<SelectTrigger>
+											<SelectValue placeholder="Map" />
+										</SelectTrigger>
+										<Button
+											variant="ghost"
+											size="icon"
+											disabled={!map}
+											onClick={() => handleFilterDelete('map')}
+										>
+											<Icon name="x" />
+											<span className="sr-only">clean map</span>
+										</Button>
+									</div>
+									<SelectContent>
 										<SelectGroup>
-											<SelectLabel>Inactive</SelectLabel>
-											{inactiveMaps.map((m) => (
+											<SelectLabel>Active</SelectLabel>
+											{activeMaps.map((m) => (
 												<SelectItem key={m.label} value={m.name}>
 													<div className="flex items-center gap-2 truncate text-sm">
 														<img
@@ -156,117 +149,134 @@ export function ContentFilter({ hideFilter }: ContentFilterProps) {
 												</SelectItem>
 											))}
 										</SelectGroup>
-									) : null}
-								</SelectContent>
-							</Select>
-						)}
-						{hideFilter?.team ? null : (
-							<Select
-								value={team}
-								onValueChange={(v) => handleFilterChange('team', v)}
-							>
-								<div className="flex">
-									<SelectTrigger>
-										<SelectValue placeholder="Team" />
-									</SelectTrigger>
-									<Button
-										variant="ghost"
-										size="icon"
-										disabled={!team}
-										onClick={() => handleFilterDelete('team')}
-									>
-										<Icon name="x" />
-										<span className="sr-only">clear team</span>
-									</Button>
-								</div>
-								<SelectContent>
-									{teams.map((t) => (
-										<SelectItem key={t} value={t}>
-											<div className="flex items-center gap-2 truncate">
-												<img
-													src={`/img/teams/${t}.png`}
-													alt=""
-													className="size-4"
-												/>
-												{teamLabels[t]}
-											</div>
-										</SelectItem>
-									))}
-								</SelectContent>
-							</Select>
-						)}
-						{hideFilter?.type ? null : (
-							<Select
-								value={type}
-								onValueChange={(v) => handleFilterChange('type', v)}
-							>
-								<div className="flex">
-									<SelectTrigger>
-										<SelectValue placeholder="Grenade type" />
-									</SelectTrigger>
-									<Button
-										variant="ghost"
-										size="icon"
-										disabled={!type}
-										onClick={() => handleFilterDelete('type')}
-									>
-										<Icon name="x" />
-										<span className="sr-only">clear grenade type</span>
-									</Button>
-								</div>
-								<SelectContent>
-									{grenadeTypes.map((g) => (
-										<SelectItem key={g} value={g}>
-											<div className="flex items-center gap-2 truncate">
-												<img
-													src={`/img/grenades/${g}.png`}
-													alt=""
-													className="size-4"
-												/>
-												{grenadeLabels[g]}
-											</div>
-										</SelectItem>
-									))}
-								</SelectContent>
-							</Select>
-						)}
-						{hideFilter?.verified ? null : (
-							<Select
-								value={verified}
-								onValueChange={(v) => handleFilterChange('verified', v)}
-							>
-								<div className="flex">
-									<SelectTrigger>
-										<SelectValue placeholder="Verified" />
-									</SelectTrigger>
-									<Button
-										variant="ghost"
-										size="icon"
-										disabled={!verified}
-										onClick={() => handleFilterDelete('verified')}
-									>
-										<Icon name="x" />
-										<span className="sr-only">clear verified</span>
-									</Button>
-								</div>
-								<SelectContent>
-									<SelectItem value="true">Verified</SelectItem>
-									<SelectItem value="false">Unverified</SelectItem>
-								</SelectContent>
-							</Select>
-						)}
-					</div>
-					<Button
-						variant="ghost"
-						className="self-end"
-						onClick={() => {
-							handleFIlterReset()
-						}}
-					>
-						Reset
-					</Button>
-				</AccordionContent>
-			</AccordionItem>
-		</Accordion>
+										{inactiveMaps.length > 0 ? (
+											<SelectGroup>
+												<SelectLabel>Inactive</SelectLabel>
+												{inactiveMaps.map((m) => (
+													<SelectItem key={m.label} value={m.name}>
+														<div className="flex items-center gap-2 truncate text-sm">
+															<img
+																src={`/resources/map-logos/${m.logo?.id}`}
+																alt=""
+																className="size-4"
+															/>
+															{m.label}
+														</div>
+													</SelectItem>
+												))}
+											</SelectGroup>
+										) : null}
+									</SelectContent>
+								</Select>
+							)}
+							{hideFilter?.team ? null : (
+								<Select
+									value={team}
+									onValueChange={(v) => handleFilterChange('team', v)}
+								>
+									<div className="flex">
+										<SelectTrigger>
+											<SelectValue placeholder="Team" />
+										</SelectTrigger>
+										<Button
+											variant="ghost"
+											size="icon"
+											disabled={!team}
+											onClick={() => handleFilterDelete('team')}
+										>
+											<Icon name="x" />
+											<span className="sr-only">clear team</span>
+										</Button>
+									</div>
+									<SelectContent>
+										{teams.map((t) => (
+											<SelectItem key={t} value={t}>
+												<div className="flex items-center gap-2 truncate">
+													<img
+														src={`/img/teams/${t}.png`}
+														alt=""
+														className="size-4"
+													/>
+													{teamLabels[t]}
+												</div>
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+							)}
+							{hideFilter?.type ? null : (
+								<Select
+									value={type}
+									onValueChange={(v) => handleFilterChange('type', v)}
+								>
+									<div className="flex">
+										<SelectTrigger>
+											<SelectValue placeholder="Grenade type" />
+										</SelectTrigger>
+										<Button
+											variant="ghost"
+											size="icon"
+											disabled={!type}
+											onClick={() => handleFilterDelete('type')}
+										>
+											<Icon name="x" />
+											<span className="sr-only">clear grenade type</span>
+										</Button>
+									</div>
+									<SelectContent>
+										{grenadeTypes.map((g) => (
+											<SelectItem key={g} value={g}>
+												<div className="flex items-center gap-2 truncate">
+													<img
+														src={`/img/grenades/${g}.png`}
+														alt=""
+														className="size-4"
+													/>
+													{grenadeLabels[g]}
+												</div>
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+							)}
+							{hideFilter?.verified ? null : (
+								<Select
+									value={verified}
+									onValueChange={(v) => handleFilterChange('verified', v)}
+								>
+									<div className="flex">
+										<SelectTrigger>
+											<SelectValue placeholder="Verified" />
+										</SelectTrigger>
+										<Button
+											variant="ghost"
+											size="icon"
+											disabled={!verified}
+											onClick={() => handleFilterDelete('verified')}
+										>
+											<Icon name="x" />
+											<span className="sr-only">clear verified</span>
+										</Button>
+									</div>
+									<SelectContent>
+										<SelectItem value="true">Verified</SelectItem>
+										<SelectItem value="false">Unverified</SelectItem>
+									</SelectContent>
+								</Select>
+							)}
+						</div>
+						<Button
+							variant="ghost"
+							className="self-end"
+							onClick={() => {
+								handleFIlterReset()
+							}}
+						>
+							Reset
+						</Button>
+					</CardContent>
+				</Card>
+			</CollapsibleContent>
+		</Collapsible>
 	)
 }
