dfe9b279f9f361acfd6c143baf66966481b5dd06
Hitusss <kuba.niespielak@gmail.com>
2024-12-06 20:52:50 +0100

    feat: wrap SearchBar form in search tag

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -30,36 +30,38 @@ export function SearchBar({
 	}, 400)
 
 	return (
-		<Form
-			method="GET"
-			action="/users"
-			className="flex flex-wrap items-center justify-center gap-2"
-			onChange={(e) => autoSubmit && handleFormChange(e.currentTarget)}
-		>
-			<div className="flex-1">
-				<Label htmlFor={id} className="sr-only">
-					Search
-				</Label>
-				<Input
-					type="search"
-					name="search"
-					id={id}
-					defaultValue={searchParams.get('search') ?? ''}
-					placeholder="Search"
-					className="w-full"
-					autoFocus={autoFocus}
-				/>
-			</div>
-			<div>
-				<StatusButton
-					type="submit"
-					status={isSubmitting ? 'pending' : status}
-					className="flex w-full items-center justify-center"
-				>
-					<Icon name="search" size="md" />
-					<span className="sr-only">Search</span>
-				</StatusButton>
-			</div>
-		</Form>
+		<search>
+			<Form
+				method="GET"
+				action="/users"
+				className="flex flex-wrap items-center justify-center gap-2"
+				onChange={(e) => autoSubmit && handleFormChange(e.currentTarget)}
+			>
+				<div className="flex-1">
+					<Label htmlFor={id} className="sr-only">
+						Search
+					</Label>
+					<Input
+						type="search"
+						name="search"
+						id={id}
+						defaultValue={searchParams.get('search') ?? ''}
+						placeholder="Search"
+						className="w-full"
+						autoFocus={autoFocus}
+					/>
+				</div>
+				<div>
+					<StatusButton
+						type="submit"
+						status={isSubmitting ? 'pending' : status}
+						className="flex w-full items-center justify-center"
+					>
+						<Icon name="search" size="md" />
+						<span className="sr-only">Search</span>
+					</StatusButton>
+				</div>
+			</Form>
+		</search>
 	)
 }
